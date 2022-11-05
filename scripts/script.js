67f9a33d5f2a7f6ff13c6f7572ab2289abc0cc98
K3A101 <keisha.alexander@hva.nl>
2022-11-05 15:39:14 +0100

    Interactie toevoegen met Javascript

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,98 @@
 // JavaScript Document
 console.log("Howdy!");
 
-// Variabele maken voor de Li, en de image
+// VARIABELEN
+// Variabele voor de speech bubble
 const deButton = document.querySelectorAll("article:nth-of-type(2) li>button");
-console.log(deButton);
-const bubbles = document.querySelectorAll("article:nth-of-type(2) li>section")
+const bubbles = document.querySelectorAll("article:nth-of-type(2) li>section");
 
+//Variabele voor de power up menu
+var tanookiButton = document.querySelector("footer  li:nth-of-type(4");
+var vuurBloembutton = document.querySelector("footer  li:nth-of-type(2");
+var mushroomButtom = document.querySelector("footer  li:nth-of-type(1)");
+
+// Variabel voor de joystick
+var joystickNintendo = document.querySelector("article:first-of-type>section>button");
+
+
+// EVENTLISTENERS
+// Click event op de grote mario afbeelding
+tanookiButton.addEventListener("click", marioTanooki);
+vuurBloembutton.addEventListener("click", marioPyroman);
+mushroomButtom.addEventListener("click", marioWordtGroter);
+joystickNintendo.addEventListener("click", playAnimatieMetControllers );
+
+// FUNCTIES
+// Functie voor de Speech Bubble
 deButton.forEach(button => {
     button.addEventListener("click", speechBubble);
-
     function speechBubble() {
         bubbles.forEach(bubble => {
-            bubble.classList.toggle("open");
+            bubble.classList.toggle("openBubble");
         });
     }
-
 });
+
+// Functie voor de Grote afbeelding
+function marioTanooki(){
+    var groteMarioAfbeelding = document.querySelector("article:first-of-type img");
+    groteMarioAfbeelding.classList.toggle("nieuwe-afbeelding");
+     var deH1 = document.querySelector("article:first-of-type h1");
+     if (groteMarioAfbeelding.classList.contains("nieuwe-afbeelding")) {
+         deH1.innerHTML = "Tanooki Mario"
+     } else {
+         deH1.innerHTML = "Mario Oddessy"
+     }
+}
+
+//Functie van Mario in vuur Pak
+function marioPyroman(){
+      var tweedemarioAfbeelding = document.querySelector("article:first-of-type img");
+      tweedemarioAfbeelding.classList.toggle("tweede-afbeelding");
+      var deTweedeTitel = document.querySelector("article:first-of-type h1");
+      if(tweedemarioAfbeelding.classList.contains("tweede-afbeelding")){
+        deTweedeTitel.innerHTML = "Pyro Mario";
+      } else {
+          deTweedeTitel.innerHTML = "Mario Oddessy";
+      }
+}
+
+// Functie Mario Wordt groter
+
+function marioWordtGroter() {
+    var derdeAfbeelding = document.querySelector("article:first-of-type img");
+    derdeAfbeelding.classList.toggle("derde-afbeelding");
+}
+
+// Functie dat stop de springende Mario animatie
+function playAnimatieMetControllers() {
+   var deAnimatie = document.querySelector(" .wrapper img");
+   var deWrapper = document.querySelector(".wrapper");
+   deAnimatie.classList.toggle("pauseer-animatie");
+   deWrapper.classList.toggle("pauseer-animatie");
+}
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  var deButton = document.querySelector("article:nth-of-type(2) li.open");
+
+//  deButton.addEventListener("click", (e)=>{
+//     if (e.target.matches("article:nth-of-type(2) li>section"))
+//  })
 
 
 
